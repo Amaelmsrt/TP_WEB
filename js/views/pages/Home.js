fe3d41d80693d4f4d49000e92fe6aed722f20408
seb_grtd <sebastien.gratade@etu.univ-orleans.fr>
2024-03-21 09:30:01 +0100

    on a les cartes avec les images et ça vient de allahcharacters

--- js/views/pages/Home.js
+++ js/views/pages/Home.js
@@ -1,28 +1,11 @@
-// Instantiate API
-import GameDataProvider from "./../../services/GameDataProvider.js";
+
+import AllCharacters from "./AllCharacters.js";
 
 export default class Home {
 
     async render() {
-        let characters = await GameDataProvider.fetchCharacters()
-        let html = characters.map(character =>
-            /*html*/`
-            <div class="col">
-            <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">${character.nom}</text></svg>
-                <div class="card-body">
-                    <p class="card-text">${character.role}</p>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <div class="btn-group">
-                        <a href="#/characters/${character.id}" class="btn btn-sm btn-outline-secondary">+ détail sur ${character.nom}</a>
-                        </div>
-                        <small class="text-body-secondary">${character.id}</small>
-                    </div>
-                </div>
-            </div>
-            </div>
-            `
-        ).join('\n ');
+        let allCharacters = new AllCharacters();
+        let html = await allCharacters.render();
         
         return /*html*/`
             <section class="py-5 text-center container">
@@ -34,10 +17,7 @@ export default class Home {
                     </div>
                 </div>
             </section>
-            <h2>Les premiers personnages</h2>
-            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                ${html}
-            </div>
+            ${html}
         `;
     }
 }
